0dc78f39db9c9b603f3bedcdb5873e83b7de7e04
elipeF <matej6543211@gmail.com>
2020-11-01 16:41:35 +0100

    add new validators to support new wrapper functions

--- src/bot.ts
+++ src/bot.ts
@@ -6,6 +6,7 @@ import {
   IsNumber,
   IsIn,
   IsUUID,
+  IsBoolean,
 } from 'class-validator';
 
 export class BotSettings {
@@ -15,6 +16,9 @@ export class BotSettings {
   @IsOptional() @IsString() @MinLength(3) @MaxLength(30) readonly name;
   @IsOptional() @IsNumber() readonly channel;
   @IsOptional() readonly address;
+  @IsOptional() @IsBoolean() readonly commander;
+  @IsOptional() @IsNumber() readonly volume;
+  @IsOptional() @IsString() readonly song;
   @IsOptional() @IsNumber() readonly count;
   @IsOptional() @IsUUID() readonly owner;
 }
